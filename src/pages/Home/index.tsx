93e691d4d45872a8a0557440fb4f00bbb3be76c9
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-10-13 19:42:30 -0300

    adding page Home and styling him

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
-import { Container } from './styles'
+import { Container, Content, Location, EnterApp, Main } from './styles'
+
+import logoImg from '../../assets/logo.svg';
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <h1>Hello NLW</h1>
+      <Content>
+        <img src={logoImg} alt="Happy"></img>
+
+        <Main>
+          <h1>Leve felicidade para o mundo </h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </Main>
+
+        <Location>
+          <strong>Paraíba</strong>
+          <span>João Pessoa</span>
+        </Location>
+
+
+        <EnterApp>
+          >
+        </EnterApp>
+      </Content>
     </Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,86 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
   background: linear-gradient(329.54deg, #29B6D1 0%, #00C7C7 100%);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  position: relative;
+  width: 100vw;
+  max-width: 1100px;
+
+  height: 100vh;
+  max-height: 680px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+export const Location = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: right;
+
+  position: absolute;
+  right: 0;
+  top: 0;
+
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 34px;
+
+  strong {
+    font-weight: 800;
+  }
+`;
+
+export const EnterApp = styled.a`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+
+  width: 80px;
+  height: 80px;
+
+  background: #FFD666;
+  color: #8D734B;
+  border-radius: 30px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: #96FEFF;
+    color: #15C3D6;
+  }
+`;
+
+export const Main = styled.main`
+  max-width: 350px;
+  max-height: 500px;
+
+  h1 {
+    font-size: 76px;
+    font-weight: 900;
+    line-height: 70px;
+  }
+
+  p {
+    margin-top: 40px;
+    max-height: 68px;
+    font-size: 24px;
+    line-height: 34px;
+    font-weight: 600;
+  }
+
 `;
