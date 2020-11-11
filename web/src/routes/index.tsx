de49364a289144c87effc4e3ad3d6ac393e8f011
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-11-10 21:47:20 -0300

    adding routes to create and retrieve a orphanage

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
-import Home from '../pages/Home/index'
-import OrphanagesMap from '../pages/OrphanagesMap'
+import Home from '../pages/Home/index';
+import OrphanagesMap from '../pages/OrphanagesMap';
+import Orphanage from '../pages/DetailOrphanage';
+import CreateOrphanages from '../pages/CreateOrphanage';
+
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/app" component={OrphanagesMap} />
+
+    <Route path="/orphanages/create" component={CreateOrphanages} />
+    <Route path="/orphanages/:id" component={Orphanage} />
   </Switch>
 )
 
